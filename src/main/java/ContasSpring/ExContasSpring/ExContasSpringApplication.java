9f3210bd941e2b4ce5582232304ba22e418e0c81
André Bueno <andrebueno99@gmail.com>
2023-05-15 18:20:22 -0300

    Upload arquivos

---
+++ src/main/java/ContasSpring/ExContasSpring/ExContasSpringApplication.java
@@ -0,0 +1,13 @@
+package ContasSpring.ExContasSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExContasSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExContasSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ContasSpring/ExContasSpring/ExContasSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package ContasSpring.ExContasSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExContasSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
